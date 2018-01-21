0ff939a52f3437f8d7f30b91cba554fe4eb78278
JamiePed <jamie.ped.peacock@gmail.com>
2018-01-21 05:11:52 +0000

    Add Variables to Example

--- src/graphiql.example/GraphQl/GraphQlQuery.cs
+++ src/graphiql.example/GraphQl/GraphQlQuery.cs
@@ -1,10 +1,12 @@
-﻿namespace graphiql.example.GraphQl
+﻿using GraphQL;
+
+namespace graphiql.example.GraphQl
 {
     public class GraphQlQuery
     {
         public string OperationName { get; set; }
         public string NamedQuery { get; set; }
         public string Query { get; set; }
-        public string Variables { get; set; }
+        public Inputs Variables { get; set; }
     }
 }
