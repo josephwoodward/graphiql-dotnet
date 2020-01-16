10c057261a0d6784e7ca1c81f03f653e838e2dcb
Eduard Minasyan <mrEDitor@mail.ru>
2020-01-16 11:58:56 +0300

    Add netcore3 build variants

---
+++ src/graphiql.example_core3/GraphQl/Models/StarWarsQuery.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL.Types;
+
+namespace GraphiQl.example.GraphQl.Models
+{
+    public class StarWarsQuery : ObjectGraphType
+    {
+        public StarWarsQuery()
+        {
+            Field<DroidType>("hero", resolve: context => new Droid { Id = 1, Name = "R2-D2" });
+            Field<PersonType>("person", resolve: context => new Person { Id = 1, Name = "Mr Happy", Age = 32 });
+            
+            // Field<PersonType>("person", resolve: context => new Person { Id = 5, FirstName = "Joe", Surname = "Woodward", Age = 32 });
+        }
+    }
+}
