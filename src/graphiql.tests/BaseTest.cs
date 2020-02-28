7c9f6bf695e32c5eda076c03f280e8e4693046e4
Joseph Woodward <joseph.woodward@just-eat.com>
2020-02-28 23:17:32 +0000

    Fix?

--- src/graphiql.tests/BaseTest.cs
+++ src/graphiql.tests/BaseTest.cs
@@ -11,8 +11,19 @@ namespace GraphiQl.tests
         protected BaseTest(bool runHeadless)
         {
             var options = new ChromeOptions();
+            options.AddArgument("--remote-debugging-port=9222");
+            options.AddArgument("--no-sandbox");
             if (runHeadless)
-                options.AddArgument("--headless");
+                options.AddArgument("headless");
+            
+            /*
+            options.add_argument('headless')
+            options.add_argument('--disable-infobars')
+            options.add_argument('--disable-dev-shm-usage')
+            options.add_argument('--no-sandbox')
+            options.add_argument('--remote-debugging-port=9222')
+            */
+            
             
             Driver = new ChromeDriver(options);
         }
